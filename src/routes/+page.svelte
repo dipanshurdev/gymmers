c45de31712c70d8c1aadab494fa5eecccb2b21fd
Aman <dv451197@gmail.com>
2024-03-10 22:49:15 +0530

    building Review section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
   import Hero from "../components/Hero.svelte";
   import Product from "../components/Product.svelte";
+  import Reviews from "../components/Reviews.svelte";
 </script>
 
 <main class="flex flex-col">
   <Hero />
   <Product />
+  <Reviews />
 </main>
