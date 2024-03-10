72622ae59ae4e6d19c6da3fa5a074e60c19a3708
Aman <dv451197@gmail.com>
2024-03-10 17:16:03 +0530

    done: hero rating stars

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -2,6 +2,24 @@
   import CtAs from "./CTAs.svelte";
   import Header from "./Header.svelte";
   import Wrapper from "./Wrapper.svelte";
+
+  const ratings = [
+    {
+      icon: "fa-solid fa-star text-amber-400",
+    },
+    {
+      icon: "fa-solid fa-star text-amber-400",
+    },
+    {
+      icon: "fa-solid fa-star text-amber-400",
+    },
+    {
+      icon: "fa-solid fa-star text-amber-400",
+    },
+    {
+      icon: "fa-solid fa-star-half text-amber-400",
+    },
+  ];
 </script>
 
 <Wrapper id="home">
@@ -10,7 +28,7 @@
     class="flex flex-col flex-1 max-w-[1400px] mx-auto w-full items-center justify-center"
   >
     <h2
-      class=" text-5xl sm:text-6xl md:text-7xl lg:text-8xl max-w-[1200px] mx-auto w-full text-center font-semibold"
+      class=" text-4xl sm:text-6xl md:text-6xl lg:text-8xl max-w-[1400px] mx-auto w-full text-center font-semibold"
     >
       <span class="text-blue-500"> GYMMING </span>
       Is
@@ -20,7 +38,7 @@
     </h2>
     <p
       class="text-xl sm:text-2xl md:text-3xl
-     text-center max-w-[1000px] mx-auto w-full text-slate-700"
+     text-center max-w-[1200px] mx-auto w-full text-slate-700 my-4"
     >
       A dynamic image capturing the <span
         class="italic text-slate-900 font-bold capitalize"
@@ -28,20 +46,18 @@
         energy and enthusiasm of individuals engaged in various fitness
         activities at Gymmers.
       </span>
-      This could include people lifting weights, doing yoga, participating in group
-      classes, and using cardio equipment, all with bright smiles and determination
-      on their faces.
+      This could include people lifting weights, participating in group , and using
+      cardio equipment, all with bright smiles on their faces.
     </p>
     <CtAs />
-    <div class="flex items-center justify-center gap-2 text-base">
+    <div class="flex items-center justify-center text-base w-2/4 flex-row py-4">
       <p>4,6</p>
-      {#each Array.from({ length: 5 }) as index}
-        <div class="grid place-items-center relative">
-          <i class="fa-solid fa-star opacity-0"></i>
+      {#each ratings as star}
+        <div class="grid place-items-center relative w-5 h-3">
           <div
-            class={`absolute top-0 left-0 grid place-items-center ${index === 4 ? "w-[50%] overflow-hidden " : ""}`}
+            class="absolute top-0 left-0 grid place-items-center pr-2 justify-center w-8"
           >
-            <i class="fa-solid fa-star text-amber-400"></i>
+            <i class={star.icon}></i>
           </div>
         </div>
       {/each}
