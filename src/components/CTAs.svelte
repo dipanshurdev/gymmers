da1b7e0447e675e997908fc48823fcc7da785616
Aman <dv451197@gmail.com>
2024-03-11 22:20:20 +0530

    update footer

--- src/components/CTAs.svelte
+++ src/components/CTAs.svelte
@@ -4,7 +4,7 @@
 
 <div class="flex items-center flex-center gap-4">
   <button class={"specialBtn" + (dark ? "bg-[#181b34]" : "")}>
-    <p class="text-base sm:text-lg md:text-xl">Contact us</p>
+    <p class="text-base sm:text-lg md:text-xl specialBtn">Contact us</p>
   </button>
   <button class="specialBtnDark">
     <p class="text-base sm:text-lg md:text-xl">Get Started &rarr;</p></button
--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,33 +3,44 @@
     class="max-w-[1200px] mx-auto w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-8 text-base"
   >
     <div class="flex flex-col gap-4 md:col-span-2">
-      <h1 class="font-semibold">
+      <h1 class="font-semibold text-3xl max-sm:text-base">
         Gym<span class="text-blue-500">mers</span>
       </h1>
       <p>&copy; 2024 Gymmers LTD, All rights reserved.</p>
     </div>
     <div class="flex-col gap-4 flex">
-      <p class="font-bold text-base sm:text-lg">Support</p>
+      <p class="font-bold text-base sm:text-lg underline">
+        <i class="fa-solid fa-headset"></i>
+        Support
+      </p>
       <a
         href="/"
         target="_blank"
         class="cursor-pointer hover:text-blue-500 duration-200"
       >
+        <i class="fa-solid fa-id-badge"></i>
         Contact Us
       </a>
     </div>
     <div class="flex-col gap-4 flex">
-      <p class="font-bold text-base sm:text-lg">Research</p>
+      <p class="font-bold text-base sm:text-lg underline">
+        <i class="fa-solid fa-magnifying-glass"></i>
+        Research
+      </p>
       <a
         href="/"
         target="_blank"
         class="cursor-pointer hover:text-blue-500 duration-200"
       >
+        <i class="fa-regular fa-file"></i>
         Read the Docs
       </a>
     </div>
     <div class="flex-col gap-4 flex">
-      <p class="font-bold text-base sm:text-lg">Follow Us</p>
+      <p class="font-bold text-base sm:text-lg underline">
+        <i class="fa-solid fa-user-plus"></i>
+        Follow Us
+      </p>
       <a
         href="/"
         target="_blank"
@@ -59,7 +70,7 @@
         target="_blank"
         class="cursor-pointer hover:text-blue-500 duration-200"
       >
-        <i class="fa-solid fa-envelope-open-text"></i>
+        <i class="fa-solid fa-envelope-open-text pr-2"></i>
         Mail Us
       </a>
     </div>
