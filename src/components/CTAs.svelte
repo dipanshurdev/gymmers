e956f142f9d54b2f840f98ea57035e8c4dd00da7
Aman <dv451197@gmail.com>
2024-03-11 22:24:01 +0530

    fix button style

--- src/components/CTAs.svelte
+++ src/components/CTAs.svelte
@@ -3,8 +3,11 @@
 </script>
 
 <div class="flex items-center flex-center gap-4">
-  <button class={"specialBtn" + (dark ? "bg-[#181b34]" : "")}>
-    <p class="text-base sm:text-lg md:text-xl specialBtn">Contact us</p>
+  <button
+    class={"specialBtn" +
+      (dark ? "bg-[#181b34] border border-slate-50 rounded-lg px-5 py-2" : "")}
+  >
+    <p class="text-base sm:text-lg md:text-xl">Contact us</p>
   </button>
   <button class="specialBtnDark">
     <p class="text-base sm:text-lg md:text-xl">Get Started &rarr;</p></button
